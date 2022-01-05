f795d6834f440bf2960fac284c55aff1d12521bf
Zach Hilman <me@zachhilman.net>
2022-01-05 12:47:58 -0500

    Add function calls to test

--- tests/driver/driver.cxx
+++ tests/driver/driver.cxx
@@ -1,6 +1,9 @@
 #include <date/date.h>
 #include <date/tz.h>
+#include <iostream>
 
 int main() {
+  auto x = date::year{2021}/date::January/1;
+  auto t = make_zoned(date::current_zone(), std::chrono::system_clock::now());
   return 0;
 }

d86bf8057676f9ff76ae6ee9c3f631ec4a34d73a
Zach Hilman <me@zachhilman.net>
2022-01-07 12:59:07 -0500

    Revision 2: Remove TZ tests due to download

--- tests/driver/driver.cxx
+++ tests/driver/driver.cxx
@@ -1,9 +1,7 @@
 #include <date/date.h>
-#include <date/tz.h>
 #include <iostream>
 
 int main() {
   [[maybe_unused]] auto x = date::year{2021}/date::January/1;
-  [[maybe_unused]] auto t = make_zoned(date::current_zone(), std::chrono::system_clock::now());
   return 0;
 }

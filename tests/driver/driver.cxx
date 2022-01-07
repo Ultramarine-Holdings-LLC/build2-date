2712270170b4aee993392721d4e1fcdc9320619a
Zach Hilman <me@zachhilman.net>
2022-01-07 15:01:06 -0500

    Revision 3: Add optional TZ test

--- tests/driver/driver.cxx
+++ tests/driver/driver.cxx
@@ -1,7 +1,13 @@
 #include <date/date.h>
+#ifdef TEST_TZ
+#include <date/tz.h>
+#endif
 #include <iostream>
 
 int main() {
   [[maybe_unused]] auto x = date::year{2021}/date::January/1;
+#ifdef TEST_TZ
+  [[maybe_unused]] auto t = make_zoned(date::current_zone(), std::chrono::system_clock::now());
+#endif
   return 0;
 }
